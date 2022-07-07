85f7fba1a318435e5cb03309096cf7dd6681a4cb
Okan Aktaş <okanaktas02@gmail.com>
2022-07-07 17:49:00 +0300

    Update script.js

--- Calisma1/script.js
+++ Calisma1/script.js
@@ -1,5 +1,5 @@
-function sayHello(){
-    console.log("Hello World");
-}
+let deger1= prompt(`Birinci degeri giriniz: `);
+let deger2 =prompt(`İkinci degeri giriniz: `);
 
-sayHello();
+document.write(`Girilen Birinci Deger: ${deger1}
+Girilen İkinci Deger: ${deger2} ve Degerler Toplamı: ${2*(parseInt(deger1)+parseInt(deger2))}`);
